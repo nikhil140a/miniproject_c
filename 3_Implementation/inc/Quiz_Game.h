18d1bcb0a11774edb36c645c0a93ee7ba9581885
NIKHIL PANDEY <81204066+nikhil140a@users.noreply.github.com>
2021-04-15 17:27:45 +0530

    Add files via upload

---
+++ 3_Implementation/inc/Quiz_Game.h
@@ -0,0 +1,28 @@
+#ifndef __Quiz_Game_H__
+#define __Quiz_Game_H__
+
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+#include<string.h>
+
+void showrecord();
+
+
+
+void resetscore();
+
+
+
+void help();
+
+
+
+
+
+void editscore(float , char []);
+
+
+
+
+#endif
