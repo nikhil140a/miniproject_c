84a4fe4f99f424147c122ac12a49481aa3c68276
NIKHIL PANDEY <81204066+nikhil140a@users.noreply.github.com>
2021-04-16 13:47:57 +0530

    Update editscore.c

--- 3_Implementation/src/editscore.c
+++ 3_Implementation/src/editscore.c
@@ -3,17 +3,19 @@
 
 void editscore(float score, char plnm[20])
 	{
-    system("cls");
-	float sc;
+        system("cls");
+	float scr1;
 	char nm[20];
 	FILE *f;
 	f=fopen("score.txt","r");
-	fscanf(f,"%s%f",&nm,&sc);
-	if (score>=sc)
-	  { sc=score;
+	fscanf(f,"%s%f",&nm,&scr1);
+	if (score>=scr1)
+	  { 
+            scr1=score;
 	    fclose(f);
 	    f=fopen("score.txt","w");
-	    fprintf(f,"%s\n%.2f",plnm,sc);
+	    fprintf(f,"%s\n%.2f",plnm,scr1);
 	    fclose(f);
-      }
-    }
+           }
+
+          }
