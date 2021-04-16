1f3e8ccf070de87e0c2a7c3958ccd0666e48c8b9
NIKHIL PANDEY <81204066+nikhil140a@users.noreply.github.com>
2021-04-16 13:46:59 +0530

    Update Instructions.c

--- 3_Implementation/src/Instructions.c
+++ 3_Implementation/src/Instructions.c
@@ -1,23 +1,21 @@
 #include "Quiz_Game.h"
 
 void help()
-	{
-        system("cls");
-        printf("\n\n                              HELP");
-        printf("\n -------------------------------------------------------------------------");
-        printf("\n ......................... C program Quiz Game...........");
-        printf("\n >> There are two rounds in the game, WARMUP ROUND & CHALLANGE ROUND");
-        printf("\n >> In warmup round you will be asked a total of 3 questions to test your general");
-        printf("\n    knowledge. You will be eligible to play the game if you can give atleast 2");
-        printf("\n    right answers otherwise you can't play the Game...........");
-        printf("\n >> Your game starts with the CHALLANGE ROUND. In this round you will be asked");
-        printf("\n    total 10 questions each right answer will be awarded $100,000.");
-        printf("\n    By this way you can win upto ONE MILLION cash prize in USD...............");
-        printf("\n >> You will be given 4 options and you have to press A, B ,C or D for the");
-        printf("\n    right option");
-        printf("\n >> You will be asked questions continuously if you keep giving the right answers.");
-        printf("\n >> No negative marking for wrong answers");
+	{system("cls");
+    printf("\n\n                              HELP");
+    printf("\n");
+    printf("\n ......................... Quiz Mania...........");
+    printf("\n >> It's a two level Quiz Game, BEGINNERS ROUND & KHATARNAK ROUND");
+    printf("\n >> In BEGINNERS  ROUND there will be total of 3 questions to test your ");
+    printf("\n    knowledge. Player will be eligible to play KHATARNAK ROUND  if you can answer atleast 2");
+    printf("\n    questions otherwise you can't proceed further...........");
+    printf("\n >> Your game starts with the KHATARNAK ROUND. In KHATARNANK ROUND you will be asked");
+    printf("\n    total 10 questions each right answer will be awarded RS 10,00,000.");
+    printf("\n    Player can earn upto ONE CRORE cash prize in Rupees...............");
+    printf("\n >> Player will be given 4 options and he/she will have to press A, B ,C or D for the");
+    printf("\n    right option");
+    printf("\n >> You will be asked questions till player keeps giving the right answers.");
+    printf("\n >> No penalty for wrong answers");
 
-        printf("\n\n\t*********************BEST OF LUCK*********************************");
-        printf("\n\n\t*****C PROGRAM QUIZ GAME is developed by CODE WITH C TEAM********");
-    }
+	printf("\n\n\t********ALL THE BEST************");
+	printf("\n\n\t**THIS QUIZ GAME is developed by KODERZZZZZ***");}
