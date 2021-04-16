99186fb356d88f042f4cf5be4c3f6a5d583df959
NIKHIL PANDEY <81204066+nikhil140a@users.noreply.github.com>
2021-04-16 13:49:50 +0530

    Update show record.c

--- 3_Implementation/src/show record.c
+++ 3_Implementation/src/show record.c
@@ -3,14 +3,14 @@
 void showrecord()
     {
         system("cls");
-        char name[20];
-        float scr;
-        FILE *f;
-        f=fopen("score.txt","r");
-        fscanf(f,"%s%f",&name,&scr);
-        printf("\n\n\t\t*************************************************************");
-        printf("\n\n\t\t %s has secured the Highest Score %0.2f",name,scr);
-        printf("\n\n\t\t*************************************************************");
-        fclose(f);
-        getch();
-    }
+	char name[20];
+	float sc;
+	FILE *f;
+	f=fopen("score.txt","r");
+	fscanf(f,"%s%f",&name,&sc);
+	printf("\n\n\t\t~~~~~~~~~~~~~~~~~~~~~");
+	printf("\n\n\t\t %s  Highest Scorer %0.2f",name,sc);
+	printf("\n\n\t\t*********************");
+	fclose(f);
+	getch();
+     }
